fa46ff7af333126056931435456cc0ba532b3499
Hongli Xu <wu402a8zc7@gmail.com>
2020-01-08 17:26:12 -0500

    basic login component

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, Input, FormItem } from 'element-ui'
 import lang from 'element-ui/lib/locale/lang/en'
 import locale from 'element-ui/lib/locale'
 
 locale.use(lang)
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(Input)
+Vue.use(FormItem)
