8cd98e4b2a68966ff3ed09d606fd092e13652243
Hongli Xu <wu402a8zc7@gmail.com>
2020-01-12 22:16:37 -0500

    continue working on Roles component

---
+++ src/components/power/Roles.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card>
+      <el-row>
+        <el-col>
+          <!-- 添加角色按钮区域 -->
+          <el-button type="primary">添加角色</el-button>
+        </el-col>
+      </el-row>
+
+      <!-- 角色列表区域 -->
+      <el-table :data="roleList" border stripe>
+        <el-table-column type="expand">
+          <template v-slot="scope">
+            <el-row v-for="(item1, i1) in scope.row.children" :key="item1.id" :class="['bdbottom', i1 === 0 ? 'bdtop' : '']" class="vcenter"> 
+              <!-- 渲染一级权限 -->
+              <el-col :span="5">
+                <el-tag closable @close="removeRightById(scope.row, item1.id)">{{item1.authName}}</el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <!-- 渲染二级和三级权限 -->
+              <el-col :span="19">
+                <el-row v-for="(item2, i2) in item1.children" :key="item2.id" :class="[i2 === 0 ? '' : 'bdtop']" class="vcenter">
+                  <el-col :span="6">
+                    <el-tag type="success" closable @close="removeRightById(scope.row, item2.id)">{{item2.authName}}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col >
+                  <el-col :span="18">
+                    <el-tag v-for="(item3) in item2.children" :key="item3.id" type="warning" closable @close="removeRightById(scope.row, item3.id)">
+                      {{item3.authName}}
+                    </el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-table-column>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="角色名称" prop="roleName"></el-table-column>
+        <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+        <el-table-column label="操作">
+          <template v-slot="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+            <el-button type="warning" icon="el-icon-search" size="mini">分配权限</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        // 所有角色列表
+        roleList: []
+      }
+    },
+    created() {
+      this.getRolesList()
+    },
+    methods: {
+      // 获取所有角色列表
+      async getRolesList() {
+        const { data: res } = await this.$http.get('roles')
+        if (res.meta.status !== 200) return this.$message.error('获取角色列表失败')
+        this.roleList = res.data
+      },
+      // 根据id删除对应的权限
+      async removeRightById(role, rightID) {
+        // 弹框提示用户是否删除 
+        const confirmResult = await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).catch(err => err)
+
+        if (confirmResult !== 'confirm') return this.$message.info('取消了删除')
+
+        const { data: res } = await this.$http.delete(`roles/${role.id}/rights/${rightID}`)
+        if (res.meta.status !== 200) return this.$message.error('删除权限失败')
+        role.children = res.data
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+.el-tag {
+  margin: 7px;
+}
+
+.bdtop {
+  border-top: 1px solid #eee
+}
+
+.bdbottom {
+  border-bottom: 1px solid #eee
+}
+
+.vcenter {
+  display: flex;
+  align-items: center
+}
+</style>
